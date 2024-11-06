38d2fa3d6bf872574b9a6c5c665c6eca84842076
PRINCE TAGUILING <2200326@slu.edu.ph>
2024-11-06 10:02:33 +0800

    Initial setup of React, Bootstrap, React Router

---
+++ sports-txt/src/pages/HomePage.js
@@ -0,0 +1,14 @@
+// src/pages/HomePage.js
+import React from 'react';
+
+const HomePage = () => {
+  return (
+    <div className="container mt-4">
+      <h1>Welcome to SportsTXT</h1>
+      <p>This is the Home Page.</p>
+      {/* Add Home Page sections here */}
+    </div>
+  );
+};
+
+export default HomePage;
